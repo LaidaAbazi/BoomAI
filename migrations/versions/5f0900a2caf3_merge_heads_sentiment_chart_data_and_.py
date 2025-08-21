6801e3296ad0072b1529149cffb7fb6eeb3969c3
Laida Abazi <laida.abazi@boreksolutions.de>
2025-08-21 13:41:01 +0200

    fixx

--- migrations/versions/5f0900a2caf3_merge_heads_sentiment_chart_data_and_.py
+++ migrations/versions/5f0900a2caf3_merge_heads_sentiment_chart_data_and_.py
@@ -1,7 +1,7 @@
 """Merge heads: sentiment chart data and color field
 
 Revision ID: 5f0900a2caf3
-Revises: 
+Revises: 78374a535955, add_color_field_to_labels
 Create Date: 2025-08-21 10:42:33.455891
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '5f0900a2caf3'
-down_revision = None
+down_revision = ('78374a535955', 'add_color_field_to_labels')
 branch_labels = None
 depends_on = None
 
