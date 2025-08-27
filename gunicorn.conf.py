cd7a86c2cc22f10294bccafba9a829ed4b00722d
LaidaAbazi <125298423+LaidaAbazi@users.noreply.github.com>
2025-08-27 14:23:24 +0200

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -8,7 +8,7 @@ backlog = 2048
 workers = 2
 worker_class = "sync"
 worker_connections = 1000
-timeout = 60
+timeout = 30
 keepalive = 2
 
 # Restart workers after this many requests, to help prevent memory leaks
